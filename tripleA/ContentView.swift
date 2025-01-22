ad0b83a62b15029ca8ac4007534757ae137d5dee
Brian Sen <bsen@ucsb.edu>
2025-01-21 21:02:57 -0800

    Initial commit: Created Xcode project

---
+++ tripleA/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  tripleA
+//
+//  Created by Brian Sen on 1/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tripleA/tripleAApp.swift
@@ -0,0 +1,17 @@
+//
+//  tripleAApp.swift
+//  tripleA
+//
+//  Created by Brian Sen on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct tripleAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
